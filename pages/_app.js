0ae470255e364b95f40ccaad62a98219cdf0f72c
Rgerard96 <rashidgerard@hotmail.com>
2022-05-21 10:38:00 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import Header from '../components/Header';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='font-primary'>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;700&display=swap'
+          rel='stylesheet'
+        ></link>
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
