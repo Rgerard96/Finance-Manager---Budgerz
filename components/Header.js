def7489ffcc88802d61273727799e73239ba4440
Rgerard96 <rashidgerard@hotmail.com>
2022-05-22 14:04:58 +0200

    adjust header component

--- components/Header.js
+++ components/Header.js
@@ -6,15 +6,8 @@ export default function Header() {
   return (
     <div className='bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-5 md:px-8 text-white font-bold text-xl fixed top-0 left-0 right-0'>
       <div className='container mx-auto flex justify-between'>
-        <UserCircleIcon className=' w-7 sm:hidden' />
         <div>Budgerz</div>
-        <MenuAlt4Icon className=' w-7 sm:hidden' />
-        <div className='hidden sm:flex items-center space-x-3 font-normal'>
-          <a>category</a>
-          <a>category</a>
-          <a>category</a>
-          <a>category</a>
-        </div>
+        <MenuAlt4Icon className='w-7 cursor-pointer' />
       </div>
     </div>
   );
