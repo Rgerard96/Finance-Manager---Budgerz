a25f2118e458a3172358ffe9de7b6848bbd8b4de
Rgerard96 <rashidgerard@hotmail.com>
2022-05-22 20:53:54 +0200

    adjust manifest file

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import { UserCircleIcon } from '@heroicons/react/solid';
 
 export default function Header() {
   return (
-    <div className='text-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-5 standalone:pt-0 text-white font-bold text-xl standalone:h-22'>
+    <div className='text-center fixed bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-5 standalone:pt-0 text-white font-bold text-xl standalone:h-20'>
       Budgerz
     </div>
   );
