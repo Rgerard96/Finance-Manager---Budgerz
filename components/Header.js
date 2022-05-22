f211cf00028ab7543e270e328f456f5037a9f16b
Rgerard96 <rashidgerard@hotmail.com>
2022-05-22 21:15:43 +0200

    adjust manifest file

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className='text-center fixed top-0 left-0 right-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-5 standalone:pt-10 text-white font-bold text-xl standalone:h-20'>
+    <div className='text-center fixed top-0 left-0 right-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 py-5 standalone:pt-14 text-white font-bold text-xl standalone:h-24'>
       Budgerz
     </div>
   );
