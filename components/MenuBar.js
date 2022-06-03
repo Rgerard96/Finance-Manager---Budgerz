d411cd10cbcf8264e76eb2c895c1dbb974160bcf
Rgerard96 <rashidgerard@hotmail.com>
2022-06-03 11:35:34 +0200

    adjust header and menubar

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -18,7 +18,7 @@ export default function MenuBar() {
   return (
     <div className='fixed sm:static bg-white left-0 bottom-0 right-0 px-5 pt-5 sm:p-0 pb-5 standalone:pb-7'>
       <div
-        className={`fixed left-0 bottom-0 right-0 border-t bg-white p-5 standalone:pb-8 sm:flex-col items-center sm:items-start justify-around sm:justify-start sm:space-y-10 ${
+        className={`fixed left-0 bottom-0 right-0 border-t bg-white py-8 sm:flex-col items-center sm:items-start justify-around sm:justify-start sm:space-y-10 ${
           router.pathname === '/inloggen'
             ? 'hidden'
             : router.pathname === '/registreren'
@@ -28,25 +28,25 @@ export default function MenuBar() {
       >
         <Link href='/'>
           <div className='flex space-x-3 items-center hover:text-blue-500'>
-            <HomeIcon className='w-6' />
+            <HomeIcon className='w-7' />
           </div>
         </Link>
         <Link href='/statistieken'>
           <div className='flex space-x-3 items-center hover:text-blue-500'>
-            <ChartBarIcon className='w-6' />
+            <ChartBarIcon className='w-7' />
           </div>
         </Link>
         <div className='sm:hidden bg-gradient-to-r from-blue-500  to-red-500 rounded-full p-3 -my-3'>
-          <PlusIcon className='w-6 text-white' />
+          <PlusIcon className='w-7 text-white' />
         </div>
         <Link href='/mijnoverzicht'>
           <div className='flex space-x-3 items-center hover:text-blue-500'>
-            <CashIcon className='w-6' />
+            <CashIcon className='w-7' />
           </div>
         </Link>
         <Link href='/instellingen'>
           <div className='flex space-x-3 items-center hover:text-blue-500'>
-            <CogIcon className='w-6' />
+            <CogIcon className='w-7' />
           </div>
         </Link>
       </div>
