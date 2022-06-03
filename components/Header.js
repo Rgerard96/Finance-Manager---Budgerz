55d05e929be2ae54946cc6ffe0a39ee33f60c64d
Rgerard96 <rashidgerard@hotmail.com>
2022-06-03 11:15:00 +0200

    adjust header and menubar

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div className='text-center fixed top-0 left-0 right-0 bg-gradient-to-r from-blue-500  to-red-500 py-3 standalone:pt-10 text-white font-bold text-xl standalone:h-20'>
+    <div className='text-center fixed top-0 left-0 right-0 bg-gradient-to-r from-blue-500  to-red-500 py-3 standalone:pt-12 text-white font-bold text-xl standalone:h-20'>
       Budgerz
     </div>
   );
--- components/MenuBar.js
+++ components/MenuBar.js
@@ -18,7 +18,7 @@ export default function MenuBar() {
   return (
     <div className='fixed sm:static bg-white left-0 bottom-0 right-0 px-5 pt-5 sm:p-0 pb-5 standalone:pb-7'>
       <div
-        className={`bg-gray-100 rounded-lg p-5 border border-gray-400 sm:flex-col items-center sm:items-start justify-around sm:justify-start sm:space-y-10 ${
+        className={`fixed left-0 bottom-0 right-0 border-t bg-white p-5 sm:flex-col items-center sm:items-start justify-around sm:justify-start sm:space-y-10 ${
           router.pathname === '/inloggen'
             ? 'hidden'
             : router.pathname === '/registreren'
@@ -26,41 +26,21 @@ export default function MenuBar() {
             : 'flex'
         }`}
       >
-        <Link href='/'>
-          <div className='cursor-pointer flex space-x-3 items-center text-gray-400 hover:text-blue-500'>
-            <HomeIcon className='w-6' />
-            <span className='hidden sm:block'>
-              Home
-            </span>
-          </div>
-        </Link>
-        <Link href='/statistieken'>
-          <div className='cursor-pointer flex space-x-3 items-center text-gray-400 hover:text-blue-500'>
-            <ChartBarIcon className='w-6' />
-            <span className='hidden sm:block'>
-              Statistieken
-            </span>
-          </div>
-        </Link>
+        <div className='flex space-x-3 items-center text-gray-400 hover:text-blue-500'>
+          <HomeIcon className='w-6' />
+        </div>
+        <div className='flex space-x-3 items-center text-gray-400 hover:text-blue-500'>
+          <ChartBarIcon className='w-6' />
+        </div>
         <div className='sm:hidden bg-gradient-to-r from-blue-500  to-red-500 rounded-full p-3 -my-3'>
           <PlusIcon className='w-6 text-white' />
         </div>
-        <Link href='/mijnoverzicht'>
-          <div className='cursor-pointer flex space-x-3 items-center text-gray-400 hover:text-blue-500'>
-            <CashIcon className='w-6' />
-            <span className='hidden sm:block'>
-              Mijn overzicht
-            </span>
-          </div>
-        </Link>
-        <Link href='/instellingen'>
-          <div className='cursor-pointer flex space-x-3 items-center text-gray-400 hover:text-blue-500'>
-            <CogIcon className='w-6' />
-            <span className='hidden sm:block'>
-              Instellingen
-            </span>
-          </div>
-        </Link>
+        <div className='flex space-x-3 items-center text-gray-400 hover:text-blue-500'>
+          <CashIcon className='w-6' />
+        </div>
+        <div className='flex space-x-3 items-center text-gray-400 hover:text-blue-500'>
+          <CogIcon className='w-6' />
+        </div>
       </div>
     </div>
   );
